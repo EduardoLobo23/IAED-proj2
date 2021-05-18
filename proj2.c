cfa22cea0f21cec1a88e8f8cbe1b58463c2af01f
EduardoLobo23 <edulobo2002@gmail.com>
2021-05-18 19:24:11 +0100

    command quit

--- proj2.c
+++ proj2.c
@@ -13,9 +13,6 @@
 /* Imprime os comandos disponiveis. */
 void help();
 
-/* Termina o programa. */
-/*void quit(); */
-
 /* Adiciona ou modifica o valor a armazenar. */
 /*void set(); */
 
@@ -34,15 +31,22 @@ void help();
 /* Apaga um caminho e todos os subcaminhos. */
 /*void delete (); */
 
-void help() {
-  puts(HELP_DESCRIPTION);
-  puts(QUIT_DESCRIPTION);
-  puts(SET_DESCRIPTION);
-  puts(PRINT_DESCRIPTION);
-  puts(FIND_DESCRIPTION);
-  puts(LIST_DESCRIPTION);
-  puts(SEARCH_DESCRIPTION);
-  puts(DELETE_DESCRIPTION);
+int main() {
+    char command[10];
+    while (scanf("%s", command) && strcmp(command, "quit") != 0) {
+        if (strcmp(command, "help") == 0)
+            help();
+    }
+    return 0;
 }
 
-int main() { return 0; }
+void help() {
+    puts(HELP_DESCRIPTION);
+    puts(QUIT_DESCRIPTION);
+    puts(SET_DESCRIPTION);
+    puts(PRINT_DESCRIPTION);
+    puts(FIND_DESCRIPTION);
+    puts(LIST_DESCRIPTION);
+    puts(SEARCH_DESCRIPTION);
+    puts(DELETE_DESCRIPTION);
+}
